27c37ecfa84b6131aab0ccd35357c8a5d08b6c30
Rishabh Singh <rishabhsvats@gmail.com>
2025-01-31 14:23:53 +0530

    testing

--- k8s-deployment.sh
+++ k8s-deployment.sh
@@ -3,6 +3,7 @@
 
 
 sed -i "s#replace#${imageName}#g" k8s_deployment_service.yaml
+cat  k8s_deployment_service.yaml
 kubectl -n default get deployment ${deploymentName} > /dev/null
 
 if [[ $? -ne 0 ]]; then
